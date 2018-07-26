3d3ccbe9f7d2c2dbe0670cb2f76faace7a8436e7
xismax <guille.mulvihill@gmail.com>
2018-07-26 18:18:44 -0300

    ejercicio1 plus

--- hola-mundo/js/11-ejercicio1.js
+++ hola-mundo/js/11-ejercicio1.js
@@ -2,18 +2,25 @@
 
 /*
 programa que pida 2 numeros, que nos diga cual es el mayor, el menor y si son iguales.
+PLUS: si los numeros no son numeros o son menores o iguales a 0 que vuelva a pedir los numeros.
 */
 
-var numero1 = prompt("Ingrese el primer numero: ");
-console.log(numero1);
+var numero1 = Number(prompt("Ingrese el primer numero: "));
+var numero2 = Number(prompt("Ahora ingrese el segundo numero: "));
 
-var numero2 = prompt("Ahora ingrese el segundo numero: ");
-console.log(numero2);
+while (numero1 <= 0 || numero2 <= 0 || isNaN(numero1) || isNaN(numero2)) {
+    alert("Los valores ingresados no son correctos. Por favor reingrese los numeros");
+    numero1 = Number(prompt("Ingrese el primer numero: "));
+    numero2 = Number(prompt("Ahora ingrese el segundo numero: "));
+}
 
 if (numero1 > numero2) {
-    console.log("El primer numero ingresado es mayor que el segundo");
+    alert("El primer numero ingresado " + numero1 + " es mayor que el segundo numero ingresado " + numero2);
 } else if (numero1 == numero2) {
-    console.log("Ambos numeros ingresados son iguales");
-    } else {
-    console.log("El primer numero ingresado es menor que el segundo");
+    alert("Los numeros ingresados " + numero1 + " y " + numero2 + " son iguales");
+} else if (numero1 < numero2){
+    alert("El primer numero ingresado " + numero1 + " es menor que el segundo numero ingresado " + numero2);
+}else{
+    alert("Los valores ingresados no son correctos");
 }
+
